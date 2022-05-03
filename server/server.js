5051dc57ba446e362e79945310ed2fea676738f2
kkt9102 <63387875+kkt9102@users.noreply.github.com>
2022-05-03 16:59:52 +0900

    server.js Edit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-// React Basic Port Number is 3000
+// React local 구동 시 기본값으로 port 3000을 들고 있으니 다른 port로 설정
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// app.통신방법('/통신할 주소',(req,res).......)
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req,res) => {
     res.send('connection sussic!');
 });
+
+app.post('/idplz', (req,res) => {
+    const serverid = req.body.plzid;
+    console.log(serverid);
+})
 
 app.listen(port, ()=>{
     console.log(`Connect at http://localhost:${port}`)
